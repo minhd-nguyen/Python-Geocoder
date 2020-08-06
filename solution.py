e96436b5444d951b65566d2433597942b84e2fa8
nguyendm77 <nguyendm77@gmail.com>
2020-08-06 06:26:04 -0700

    submit hw

--- solution.py
+++ solution.py
@@ -1,5 +1,7 @@
-# import requests
+import requests
 import geocoder
+
+API_BASE_URL = "https://api.darksky.net/forecast/c73c375afbedccd447f98b4e275bb84c/"
 
 destinations = [
   "The Space Needle",
@@ -19,8 +21,17 @@ destinations = [
 for point in destinations:
   # Get the latitude and longitude from `geocoder`.
   loc = geocoder.arcgis(point)
+  full_api_url = full_api_url = API_BASE_URL + {loc.latlng[0]} + "," + {loc.latlng[1]}
+  result = requests.request('GET', full_api_url).json()
   # Print out `geopy`'s results.
   print("{0} is located at ({1:.4f}, {2: .4f})".format(point, loc.latlng[0], loc.latlng[1]))
+  print(f'At {point} right now, it is {result["currently"]["summary"]} with a temperature of {result["currently"]["temperature"]:.1f}')
+
 
 # g = geocoder.arcgis("The Space Needle")
 # print(g.latlng) # latlng is a tuple with a length of 2.
+
+# g = geocoder.arcgis('Mountain View, CA', maxRows=1)
+# print(g.geojson)
+# for result in g:
+#   print(result.address, result.latlng, result.currently)
